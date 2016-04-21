5af4a6c846dca57b3f86216624ff9901988b250e
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-04-21 12:04:16 +0200

    test_prepare on all nodes

--- bndl/compute/cassandra/tests/test_prepare.py
+++ bndl/compute/cassandra/tests/test_prepare.py
@@ -4,8 +4,6 @@ from bndl.compute.cassandra.tests import CassandraTest
 
 class QueryPrepareTest(CassandraTest):
     def test_prepare_caching(self):
-        with self.assertRaises(StopIteration):
-            self.ctx.cassandra_table(self.keyspace, self.table).first()
-        with self.assertRaises(StopIteration):
-            self.ctx.cassandra_table(self.keyspace, self.table).first()
-        self.assertEqual(self.ctx.range(4, pcount=4).map_partitions(lambda p: [_prepare.cache_info()[3]]).sum(), 1)  # @UndefinedVariable
+        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).collect(), [])
+        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).collect(), [])
+        self.assertEqual(self.ctx.range(32, pcount=16).map_partitions(lambda p: [_prepare.cache_info()[3]]).sum(), 16)  # @UndefinedVariable
