42389c061d4053b4aa0872f2fa4002109b2d4e08
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-04-27 08:48:55 +0200

    made ctx in bndl.compute.script lazy

--- bndl/compute/script.py
+++ bndl/compute/script.py
@@ -1,4 +1,4 @@
-from bndl.compute.driver import main, argparser
+from bndl.compute import driver
 from bndl.util.supervisor import  split_args
 import copy
 
@@ -6,7 +6,28 @@ import copy
 __all__ = ['ctx']
 
 
-argparser = copy.copy(argparser)
-argparser.prog = 'bndl.compute.script'
-args = argparser.parse_args(split_args())
-ctx = main(args, daemon=True)
+class LazyObject(object):
+    def __init__(self, factory):
+        self._factory = factory
+
+    def __getattribute__(self, name, *args, **kwargs):
+        try:
+            w = object.__getattribute__(self, '_wrapped')
+        except AttributeError:
+            f = object.__getattribute__(self, '_factory')
+            w = f()
+            object.__setattr__(self, '_wrapped', w)
+        return object.__getattribute__(w, name, *args, **kwargs)
+
+    def __setattribute__(self, name, *args, **kwargs):
+        return self.wrapped.__setattr__(name, *args, **kwargs)
+
+
+def create_ctx():
+    argparser = copy.copy(driver.argparser)
+    argparser.prog = 'bndl.compute.script'
+    args = argparser.parse_args(split_args())
+    return driver.main(args, daemon=True)
+
+
+ctx = LazyObject(create_ctx)
