9eb66261a2c2f171f8af7e6084e747066e04757d
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-04-29 13:06:22 +0200

    cache contact points to speed up scheduling cassandra datasets

--- bndl/compute/cassandra/session.py
+++ bndl/compute/cassandra/session.py
@@ -5,6 +5,7 @@ from bndl.compute.cassandra.loadbalancing import LocalNodeFirstPolicy
 from cassandra.cluster import Cluster, Session
 from cassandra.policies import TokenAwarePolicy
 from threading import Lock
+from itertools import chain
 
 
 _prepare_lock = Lock()
@@ -18,17 +19,28 @@ def prepare(self, query, custom_payload=None):
         return _prepare(self, query, custom_payload)
 
 
+@lru_cache()
+def get_contact_points(ctx, *contact_points):
+    if not contact_points:
+        contact_points = ctx.conf.get('cassandra.contact_points')
+    if not contact_points:
+        contact_points = set()
+        for worker in ctx.workers:
+            contact_points |= worker.ip_addresses
+    if not contact_points:
+        contact_points = ctx.node.ip_addresses
+    if isinstance(contact_points, str):
+        contact_points = contact_points.split(',')
+    return tuple(sorted(contact_points))
+
+
 @contextlib.contextmanager
 def cassandra_session(ctx, keyspace=None, contact_points=None):
     sessions = getattr(cassandra_session, 'sessions', None)
     if sessions is None:
         cassandra_session.sessions = sessions = {}
-    # determine contact points, either given or ip addresses of the local node
-    if contact_points is None:
-        contact_points = ctx.conf.get('cassandra.contact_points', ctx.node.ip_addresses)
-        if isinstance(contact_points, str):
-            contact_points = contact_points.split(',')
-    contact_points = tuple(sorted(contact_points))
+    # determine contact points, either given or ip addresses of the workers
+    contact_points = get_contact_points(ctx, *(contact_points or ()))
     # check if there is a cached session
     session = sessions.get(contact_points)
     # or create one if not or that session is shutdown
