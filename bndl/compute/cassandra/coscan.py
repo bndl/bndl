e49dd3ae57d323428c9d849eddc0eb98974300e3
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-05-13 13:41:47 +0200

    reimplemented coscan on zip

--- bndl/compute/cassandra/coscan.py
+++ bndl/compute/cassandra/coscan.py
@@ -1,28 +1,28 @@
+from bndl.util.cython import try_pyximport_install ; try_pyximport_install()
+
 import logging
 
-from bndl.compute.cassandra import partitioner
-from bndl.compute.cassandra.partitioner import estimate_size, SizeEstimate
 from bndl.compute.cassandra.session import cassandra_session
-from bndl.compute.dataset.base import Dataset, Partition
 from bndl.util.funcs import identity
+from bndl.compute.dataset.zip import ZippedDataset
 
 
 logger = logging.getLogger(__name__)
 
 
-class CassandraCoScanDataset(Dataset):
+class CassandraCoScanDataset(ZippedDataset):
     def __init__(self, *scans, keys=None, dset_id=None):
         for scan in scans[1:]:
             assert scan.contact_points == scans[0].contact_points, "only scan in parallel within the same cluster"
             assert scan.keyspace == scans[0].keyspace, "only scan in parallel within the same keyspace"
         assert len(set(scan.table for scan in scans)) == len(scans), "don't scan the same table twice"
 
-        super().__init__(scans[0].ctx, src=scans, dset_id=dset_id)
+        super().__init__(*scans, comb=self.combiner, dset_id=dset_id)
         self.contact_points = scans[0].contact_points
         self.keyspace = scans[0].keyspace
 
         # TODO check format (dicts, tuples, namedtuples, etc.)
-        # TODO adapt keyfuncs to above
+        # TODO adapt keyfuncs to below
 
         self.keys = keys
 
@@ -61,46 +61,20 @@ class CassandraCoScanDataset(Dataset):
         assert isinstance(other, CassandraScanDataset)
         return CassandraCoScanDataset(*self.src + (other,), keys=keys)
 
-    def parts(self):
-        from bndl.compute.cassandra.dataset import CassandraScanPartition
 
-        with cassandra_session(self.ctx, contact_points=self.contact_points) as session:
-            size_estimates = sum((estimate_size(session, self.keyspace, src.table) for src in self.src), SizeEstimate(0, 0, 0))
-
-            partitions = partitioner.partition_ranges(session, self.keyspace, size_estimates=size_estimates, min_pcount=self.ctx.default_pcount)
-
-            return [
-                CassandraParallelScanDataset(self, part_idx, [CassandraScanPartition(scan, part_idx, replicas, token_ranges) for scan in self.src])
-                for part_idx, (replicas, token_ranges) in enumerate(partitions)
-            ]
-
-
-
-class CassandraParallelScanDataset(Partition):
-    def __init__(self, dset, idx, children):
-        super().__init__(dset, idx)
-        self.children = children
-
-
-    def preferred_workers(self, workers):
-        return self.children[0].preferred_workers(workers)
-
-
-    def _materialize(self, ctx):
+    def combiner(self, *partitions):
         merged = {}
 
-        for cidx, child in enumerate(self.children):
-            key = self.dset.keyfuncs[cidx]
-            for row in child._materialize(ctx):
+        for cidx, child in enumerate(partitions):
+            key = self.keyfuncs[cidx]
+            for row in child:
                 k = key(row)
                 batch = merged.get(k)
                 if not batch:
-                    merged[k] = batch = [[] for _ in self.children]
+                    merged[k] = batch = [[] for _ in partitions]
                 batch[cidx].append(row)
 
         for key, groups in merged.items():
-            for idx, (group, transform) in enumerate(zip(groups, self.dset.grouptransforms)):
+            for idx, (group, transform) in enumerate(zip(groups, self.grouptransforms)):
                 groups[idx] = transform(group)
             yield key, groups
-
-        # return iter(merged.items())
