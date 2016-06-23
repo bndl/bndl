791586e7e6a2aee45a3900c92e732f09118b1af7
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-06-23 21:47:41 +0200

    generalized location of compute/datasettest

--- bndl/compute/dataset/tests/__init__.py
+++ bndl/compute/dataset/tests/__init__.py
@@ -1,15 +1,3 @@
-import unittest
+from bndl.compute.tests import ComputeTest
 
-from bndl.compute.driver import main
-
-
-class DatasetTest(unittest.TestCase):
-    worker_count = 4
-
-    @classmethod
-    def setUpClass(cls):
-        cls.ctx = main(['--workers', str(cls.worker_count)])
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.ctx.stop()
+DatasetTest = ComputeTest
---
+++ bndl/compute/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from bndl.compute.driver import main
+
+
+class ComputeTest(unittest.TestCase):
+    worker_count = 4
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ctx = main(['--workers', str(cls.worker_count)])
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.ctx.stop()
