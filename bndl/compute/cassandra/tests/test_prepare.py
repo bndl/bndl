d4ccff7c3ef81dff694a76dbbe0bec515cfb4f0e
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-06-30 14:45:49 +0200

    force hitting each worker

--- bndl/compute/cassandra/tests/test_prepare.py
+++ bndl/compute/cassandra/tests/test_prepare.py
@@ -5,8 +5,11 @@ from bndl.compute.cassandra.tests import CassandraTest
 class QueryPrepareTest(CassandraTest):
     def test_prepare_caching(self):
         # trigger query preparation
-        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).collect(), [])
-        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).collect(), [])
+        dset = self.ctx.cassandra_table(self.keyspace, self.table)
+        for i in range(self.ctx.worker_count):
+            targeted = dset.allow_workers(lambda workers: [workers[i]])
+            self.assertEqual(targeted.collect(), [])
+
         # check that prepared query is cached
         hits_count = self.ctx.range(32, pcount=16).map_partitions(lambda p: [_prepare.cache_info()[3]]).sum()
         self.assertEqual(hits_count, 16)  # @UndefinedVariable
