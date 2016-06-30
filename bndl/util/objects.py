be1f7ca33fe59c3fcec04458c718855964a9e79d
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-06-30 17:19:33 +0200

    only wrap the factory once

--- bndl/util/objects.py
+++ bndl/util/objects.py
@@ -2,14 +2,9 @@ class LazyObject(object):
     def __init__(self, factory):
         self._factory = factory
 
-    def __getattribute__(self, name, *args, **kwargs):
-        try:
-            wrapped = object.__getattribute__(self, '_wrapped')
-        except AttributeError:
-            factory = object.__getattribute__(self, '_factory')
-            wrapped = factory()
-            object.__setattr__(self, '_wrapped', wrapped)
-        return object.__getattribute__(wrapped, name, *args, **kwargs)
-
-    def __setattribute__(self, name, *args, **kwargs):
-        return self.wrapped.__setattr__(name, *args, **kwargs)
+    def __getattribute__(self, name):
+        factory = object.__getattribute__(self, '_factory')
+        obj = factory()
+        self.__class__ = obj.__class__
+        self.__dict__ = obj.__dict__
+        return object.__getattribute__(obj, name)
