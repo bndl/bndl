6809b2832533d4fe7cf37e3d427b32ef8e49874c
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-07-01 15:52:53 +0200

    check for the factory being None

--- bndl/util/objects.py
+++ bndl/util/objects.py
@@ -4,7 +4,11 @@ class LazyObject(object):
 
     def __getattribute__(self, name):
         factory = object.__getattribute__(self, '_factory')
+
         obj = factory()
+        if not obj:
+            raise RuntimeError('Unable to create lazy object from %s' % factory)
         self.__class__ = obj.__class__
         self.__dict__ = obj.__dict__
+
         return object.__getattribute__(obj, name)
