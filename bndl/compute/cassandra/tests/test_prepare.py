e471a6cab086ff19c2fcca561833e438eb48d172
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-07-01 20:58:45 +0200

    support reuse of ctx in test

--- bndl/compute/cassandra/tests/test_prepare.py
+++ bndl/compute/cassandra/tests/test_prepare.py
@@ -4,6 +4,8 @@ from bndl.compute.cassandra.tests import CassandraTest
 
 class QueryPrepareTest(CassandraTest):
     def test_prepare_caching(self):
+        hits_count_start = self.ctx.range(32, pcount=16).map_partitions(lambda p: [_prepare.cache_info()[3]]).sum()
+
         # trigger query preparation
         dset = self.ctx.cassandra_table(self.keyspace, self.table)
         for i in range(self.ctx.worker_count):
@@ -12,4 +14,4 @@ class QueryPrepareTest(CassandraTest):
 
         # check that prepared query is cached
         hits_count = self.ctx.range(32, pcount=16).map_partitions(lambda p: [_prepare.cache_info()[3]]).sum()
-        self.assertEqual(hits_count, 16)  # @UndefinedVariable
+        self.assertEqual(hits_count - hits_count_start , 16)  # @UndefinedVariable
