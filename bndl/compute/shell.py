e8a953a6a1e6fc18774c4611a6d42e8efcf27598
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-07-21 22:20:34 +0200

    disable logger from shell for now ... prints to much

--- bndl/compute/shell.py
+++ bndl/compute/shell.py
@@ -6,7 +6,6 @@ from bndl import compute
 from bndl.net.run import argparser
 from bndl.util.conf import Config
 from bndl.util.exceptions import catch
-from bndl.util.log import configure_logging
 
 
 HEADER = r''' ___ _  _ ___  _    
@@ -28,13 +27,14 @@ def main():
     try:
         args = argparser.parse_args()
         config = Config(dict(c.split('=', 1) for c in args.conf))
+
         if args.listen_addresses:
             config['bndl.net.listen_addresses'] = args.listen_addresses
         if args.seeds:
             config['bndl.net.seeds'] = args.seeds
         if args.worker_count:
             config['bndl.compute.worker_count'] = args.worker_count
-        configure_logging(log_dir=config.get('bndl.util.log.dir'))
+
         ctx = compute.create_ctx(config)
         IPython.embed(header=HEADER, user_ns=dict(ctx=ctx))
     finally:
