dbe423ecb95cf3f38a03741b658d952eb6f438c3
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-07-27 12:25:38 +0200

    accessing worker_count reinitializes the ctx (contract changed)

--- bndl/compute/tests/test_importctx.py
+++ bndl/compute/tests/test_importctx.py
@@ -5,12 +5,11 @@ class TestCtxImport(TestCase):
     def test_ctximport(self):
         from bndl.compute import ctx
 
-        self.assertTrue(ctx.await_workers() > 0)
+        worker_count = ctx.await_workers()
+        self.assertTrue(worker_count > 0)
         self.assertEqual(ctx.range(100).count(), 100)
         ctx.stop()
 
-        self.assertEquals(ctx.worker_count, 0)
-
-        self.assertTrue(ctx.await_workers() > 0)
+        self.assertEqual(ctx.await_workers(), worker_count)
         self.assertEqual(ctx.range(100).count(), 100)
         ctx.stop()
