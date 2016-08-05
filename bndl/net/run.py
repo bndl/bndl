3785ed2ed361ec0eae688a47b9df43067b5c62db
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-08-05 12:16:29 +0200

    simplify loop handling, just use the one provided

--- bndl/net/run.py
+++ bndl/net/run.py
@@ -2,7 +2,6 @@ import argparse
 import asyncio
 
 from bndl.net.connection import urlcheck
-from bndl.util import aio
 
 
 argparser = argparse.ArgumentParser()
@@ -21,21 +20,10 @@ def run_nodes(*nodes, started_signal=None, stop_signal=None):
 
     loop = nodes[0].loop
     assert all(node.loop == loop for node in nodes)
-
-    try:
-        current_loop = aio.get_loop()
-    except:
-        # set the event loop for this thread if not set yet
-        asyncio.set_event_loop(loop)
-        current_loop = loop
-
+    asyncio.set_event_loop(loop)
     loop.set_exception_handler(exception_handler)
 
-    # check that we're running on the correct loop
-    # (always the case if we've set the event loop)
     try:
-        assert loop == current_loop
-
         starts = [node.start() for node in nodes]
         loop.run_until_complete(asyncio.wait(starts, loop=loop))
         if started_signal:
