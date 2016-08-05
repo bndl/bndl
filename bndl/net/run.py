bdf30a312febf40dcd6d4be15bd5fba437fa11f4
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-08-05 10:24:17 +0200

    test the loop inside the try except block, so that the thread properly signals any waiter

--- bndl/net/run.py
+++ bndl/net/run.py
@@ -33,9 +33,9 @@ def run_nodes(*nodes, started_signal=None, stop_signal=None):
 
     # check that we're running on the correct loop
     # (always the case if we've set the event loop)
-    assert loop == current_loop
-
     try:
+        assert loop == current_loop
+
         starts = [node.start() for node in nodes]
         loop.run_until_complete(asyncio.wait(starts, loop=loop))
         if started_signal:
