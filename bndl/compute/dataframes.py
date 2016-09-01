e0ca31c0287922dc46e8e44d52f8fb10e2d1df5d
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-01 17:17:09 +0200

    steps in data frame api (notion of columns) and some utils

--- bndl/compute/dataframes.py
+++ bndl/compute/dataframes.py
@@ -1,8 +1,16 @@
-from bndl.compute.dataset import Dataset
+from functools import reduce
+
+from bndl.compute.dataset import Dataset, Partition
 import pandas as pd
 
 
-_pairwise_append = lambda a, b:a.append(b)
+def _pairwise_append(a, b):
+    return a.append(b)
+
+
+def combine_dataframes(dfs):
+    return reduce(_pairwise_append, dfs)
+
 
 
 class DataFrame(pd.DataFrame):
@@ -10,16 +18,44 @@ class DataFrame(pd.DataFrame):
         return self.itertuples(name='Row')
 
 
+
+class Columns(pd.Series):
+    def __init__(self, ddf, cols):
+        cols = [Column(ddf, *col) if isinstance(col, tuple) else col for col in cols]
+        super().__init__(cols, (col.name for col in cols))
+
+
+
+class Column(object):
+    def __init__(self, ddf, name, dtype):
+        self.ddf = ddf
+        self.name = name
+        self.dtype = dtype
+
+
+
 class DistributedDataFrame(Dataset):
-    def __init__(self, src, index, columns, dtypes):
+    def __init__(self, src, index, columns):
         super().__init__(src.ctx, src)
-        self.index = index
-        self.columns = columns
-        self.dtypes = dtypes
+        self.index = Columns(self, index)
+        self.columns = Columns(self, columns)
+
+
+    @classmethod
+    def from_sample(cls, src, sample):
+        index = sample.index
+        if isinstance(index, pd.MultiIndex):
+            index = [(level.name, level.dtype) for level in index.levels]
+        else:
+            index = [(index.name, index.dtype)]
+        return cls(src, index, sample.dtypes.items())
 
 
     def parts(self):
-        return self.src.parts()
+        return [
+            DistributedDataFramePartition(self, i, part)
+            for i, part in enumerate(self.src.parts())
+        ]
 
 
     def take(self, num):
@@ -28,7 +64,7 @@ class DistributedDataFrame(Dataset):
             df = next(heads)
             while len(df) < num:
                 try:
-                    df.append(next(heads))
+                    df = df.append(next(heads))
                 except StopIteration:
                     break
             if len(df) > num:
@@ -42,3 +78,11 @@ class DistributedDataFrame(Dataset):
         if parts:
             return super().collect(True)
         return super().glom().reduce(_pairwise_append)
+
+
+class DistributedDataFramePartition(Partition):
+    def _materialize(self, ctx):
+        data = self.src.materialize(ctx)
+        if isinstance(data, pd.DataFrame):
+            data.__class__ = DataFrame
+        return data
