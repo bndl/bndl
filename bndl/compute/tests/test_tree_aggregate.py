9c96a895e171d7e44bc66f092cfae5fe31372d56
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-09 14:48:50 +0200

    tests for tree aggregate and friends

---
+++ bndl/compute/tests/test_tree_aggregate.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from operator import add
+
+from bndl.compute.tests import DatasetTest
+
+
+class TreeAggregateTest(DatasetTest):
+    def setUp(self):
+        super().setUp()
+        self.dset = self.ctx.range(1000, pcount=100)
+
+    def test_aggregate(self):
+        self.assertEqual(self.dset.tree_aggregate(sum), self.dset.sum())
+        self.assertEqual(self.dset.tree_aggregate(Counter, lambda counters: sum(counters, Counter())),
+                         Counter(self.dset.collect()))
+
+    def test_reduce(self):
+        self.assertEqual(self.dset.tree_reduce(add), self.dset.sum())
+        self.assertEqual(self.dset.map_partitions(Counter).glom().tree_reduce(add),
+                         self.dset.count_by_value())
+
+    def test_combine(self):
+        def count(counter, value):
+            counter[value] += 1
+            return counter
+        self.assertEqual(self.dset.tree_combine(Counter(), count, add),
+                         Counter(self.dset.collect()))
+
+    def test_depth(self):
+        for d in range(2, 5):
+            with self.subTest('depth = %r' % d):
+                self.assertEqual(self.dset.tree_aggregate(sum, depth=d),
+                                 self.dset.sum())
