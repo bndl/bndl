7e3a078096e9a58cff5dc26ff9535430056ec68a
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-16 13:19:04 +0200

    don't conflict with normal bndl cluster on localhost and user less workers

--- bndl/compute/tests/__init__.py
+++ bndl/compute/tests/__init__.py
@@ -5,12 +5,13 @@ from bndl.util.conf import Config
 
 
 class ComputeTest(unittest.TestCase):
-    worker_count = 4
+    worker_count = 3
 
     @classmethod
     def setUpClass(cls):
         config = Config()
         config['bndl.compute.worker_count'] = cls.worker_count
+        config['bndl.net.listen_addresses'] = 'tcp://127.0.0.11:5000'
         cls.ctx = create_ctx(config, daemon=True)
         cls.ctx.await_workers(cls.worker_count)
 
