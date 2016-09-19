99c87acf3e31299a5ab497b338a4dc315e7014fa
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-19 18:42:06 +0100

    improve files test

--- bndl/compute/tests/test_files.py
+++ bndl/compute/tests/test_files.py
@@ -1,4 +1,4 @@
-from itertools import chain
+from itertools import chain, product
 from tempfile import TemporaryDirectory
 import gzip
 import os.path
@@ -20,6 +20,7 @@ class FilesTest(DatasetTest):
         cls.line_count = 8
         cls.file_size = cls.line_size * cls.line_count
         cls.file_count = 32
+        cls.total_line_count = cls.line_count * cls.file_count
         cls.total_size = cls.file_size * cls.file_count
         cls.contents = [
             ('\n'.join(random_string(cls.line_size - 1) for _ in range(cls.line_count)) + '\n').encode()
@@ -33,7 +34,8 @@ class FilesTest(DatasetTest):
         for contents, file in zip(cls.contents, cls.files):
             file.write(contents)
             file.flush()
-        cls.dset = cls.ctx.files(cls.filenames, psize_bytes=1024 * 2, psize_files=None).cache()
+        cls.psize_bytes = cls.file_size * 2
+        cls.dset = cls.ctx.files(cls.filenames, psize_bytes=cls.psize_bytes, psize_files=None).cache()
 
 
     @classmethod
@@ -47,35 +49,52 @@ class FilesTest(DatasetTest):
         self.assertEqual(len(self.dset.parts()), count)
 
 
+    def _test_psize(self, dset):
+        data = dset.values()
+        if dset.psize_bytes:
+            self.assertTrue(all(data.map(len).map_partitions(lambda p: (sum(p) <= dset.psize_bytes,)).collect()))
+        if dset.psize_files:
+            self.assertTrue(all(data.map_partitions(lambda p: (sum(1 for _ in p) <= dset.psize_files,)).collect()))
+
+
+    def test_psize(self):
+        psize_bytes = [None, self.file_size * 3]
+        psize_files = [None, 3]
+
+        for psize_bytes, psize_files in product(psize_bytes, psize_files):
+            with self.subTest('psize_bytes = %s, psize_files = %s' % (psize_bytes, psize_files)):
+                self._test_psize(self.ctx.files(self.filenames, psize_bytes=psize_bytes, psize_files=psize_files))
+
+
     def test_count(self):
-        self.assertEqual(self.dset.count(), 32)
+        self.assertEqual(self.dset.count(), self.file_count)
 
 
     def test_listings(self):
         dirname = self.tmpdir.name
         filename_pattern = os.path.join(dirname, 'test_file_*.tmp')
         dset = self.ctx.files(filename_pattern)
-        self.assertEqual(dset.count(), 32)
+        self.assertEqual(dset.count(), self.file_count)
         file_count = sum(1 for _ in filter(
                 lambda f: os.path.basename(f).startswith('test_file_') and f.endswith('.tmp'),
                 self.ctx.files(dirname).filenames
             )
         )
-        self.assertEqual(file_count, 32)
-        self.assertEqual(self.dset.count(), 32)
+        self.assertEqual(file_count, self.file_count)
+        self.assertEqual(self.dset.count(), self.file_count)
 
 
     def test_recursive(self):
         filter_test_files = lambda filename: os.path.basename(filename).startswith('test_file_') and filename.endswith('.tmp')
         dirname = os.path.dirname(self.filenames[0])
         dset = self.ctx.files(dirname, True, None, filter_test_files)
-        self.assertEqual(dset.count(), 32)
+        self.assertEqual(dset.count(), self.file_count)
         self.assertEqual(
             sum(1 for _ in filter(
                 filter_test_files,
                 self.ctx.files(dirname).filenames
             )
-        ), 32)
+        ), self.file_count)
 
 
     def test_binary(self):
@@ -84,12 +103,12 @@ class FilesTest(DatasetTest):
 
     def test_decode(self):
         self.assertEqual(''.join(self.dset.decode().values().collect()), ''.join(c.decode() for c in self.contents))
-        self.assertEqual(self.dset.decode().values().map(len).collect(), [128 * 8] * 32)
+        self.assertEqual(self.dset.decode().values().map(len).collect(), [self.file_size] * self.file_count)
 
 
     def test_lines(self):
-        self.assertEqual(self.dset.lines().count(), 8 * 32)
-        self.assertEqual(self.dset.lines().map(len).collect(), [127] * 8 * 32)
+        self.assertEqual(self.dset.lines().count(), self.total_line_count)
+        self.assertEqual(self.dset.lines().map(len).collect(), [self.line_size - 1] * self.total_line_count)
         self.assertEqual(self.dset.lines().collect(),
                          list(chain.from_iterable(c.decode().splitlines() for c in self.contents)))
 
@@ -106,7 +125,7 @@ class FilesTest(DatasetTest):
 
     def test_split_files(self):
         for factor in (1.9, 2.0, 2.1):
-            psize_bytes = int(1024 * factor)
+            psize_bytes = int(self.psize_bytes / 2 * factor)
             dset = self.ctx.files(self.filenames, psize_bytes=psize_bytes, psize_files=None, split=True).cache()
             for psize in dset.values().map(len).map_partitions(lambda p: [sum(p)]).collect():
                 self.assertLessEqual(psize, psize_bytes)
@@ -117,7 +136,7 @@ class FilesTest(DatasetTest):
     def test_split_fileslines(self):
         sep = '\n'
         for factor in (1.8, 1.95, 2.0, 2.2):
-            psize_bytes = int(1024 * factor)
+            psize_bytes = int(self.psize_bytes / 2 * factor)
             max_lines_per_part = psize_bytes // self.line_size
             dset = self.ctx.files(self.filenames, psize_bytes=psize_bytes, psize_files=None, split=sep).cache()
             self.assertEqual(b''.join(dset.values().collect()), b''.join(self.contents))
