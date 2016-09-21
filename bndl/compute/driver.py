069201b149a3bda67f794120bb7d7be45bd2b416
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-22 00:26:01 +0200

    cleanup

--- bndl/compute/driver.py
+++ bndl/compute/driver.py
@@ -7,9 +7,4 @@ logger = logging.getLogger(__name__)
 
 
 class Driver(Worker):
-    hosted_values = {}
-
-
-    def get_hosted_value(self, src, key):
-        logger.debug('sending broadcast value with key %s to %s', key, src)
-        return self.hosted_values[key]
+    pass
