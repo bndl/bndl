588015036dbe9e13fa0e4d3d37c9d1469dc42d31
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-23 23:24:18 +0200

    test more slice options again

--- bndl/compute/tests/test_slicing.py
+++ bndl/compute/tests/test_slicing.py
@@ -5,13 +5,15 @@ from bndl.compute.tests import DatasetTest
 
 class SlicingTest(DatasetTest):
     def test_limit(self):
-#         dset = self.ctx.collection(string.ascii_lowercase)
-#         self.assertEqual(dset.first(), string.ascii_lowercase[0])
-#         self.assertEqual(''.join(dset.take(10)), string.ascii_lowercase[:10])
-#         self.assertEqual(''.join(dset.collect()), string.ascii_lowercase)
-#
-        dset = self.ctx.collection(string.ascii_lowercase, pcount=100)
-#         self.assertEqual(dset.first(), string.ascii_lowercase[0])
+        dset = self.ctx.collection(string.ascii_lowercase)
+        self.assertEqual(dset.first(), string.ascii_lowercase[0])
         self.assertEqual(''.join(dset.take(10)), string.ascii_lowercase[:10])
-#         self.assertEqual(''.join(dset.collect()), string.ascii_lowercase)
+        self.assertEqual(''.join(dset.collect()), string.ascii_lowercase)
 
+        dset = self.ctx.collection(string.ascii_lowercase, pcount=100)
+        self.assertEqual(dset.first(), string.ascii_lowercase[0])
+        self.assertEqual(''.join(dset.take(10)), string.ascii_lowercase[:10])
+        self.assertEqual(''.join(dset.collect()), string.ascii_lowercase)
+
+    def test_next(self):
+        print(next(self.ctx.range(10).icollect(ordered=False)))
