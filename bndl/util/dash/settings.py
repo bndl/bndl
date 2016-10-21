cb18ac216b8226fa8d5466951eb1df4470518609
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-10-21 13:31:04 +0200

    cleaned up

--- bndl/util/dash/settings.py
+++ bndl/util/dash/settings.py
@@ -3,7 +3,4 @@ SECRET_KEY = 'default'
 BNDL_DASHES = [
     ('net', 'bndl.net.dash.Dash'),
     ('execute', 'bndl.execute.dash.Dash'),
-#     # ('compute', 'bndl.compute.dash.Dash'),
-#     ('cassandra', 'bndl.compute.cassandra.dash.Dash'),
-#     ('elastic', 'bndl.compute.elastic.dash.Dash'),
 ]
