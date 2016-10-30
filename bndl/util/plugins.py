9d16162cee0d6eb02a93e42d2f7ccbbefa5cbaaa
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-10-30 05:05:03 +0100

    load plugins only once

--- bndl/util/plugins.py
+++ bndl/util/plugins.py
@@ -5,12 +5,17 @@ import pkg_resources
 
 logger = logging.getLogger(__name__)
 
+_plugins = None
+
 
 def load_plugins():
-    plugins = []
+    global _plugins
+    if _plugins is not None:
+        return _plugins
+    _plugins = []
     for plugin in pkg_resources.iter_entry_points('bndl.plugin'):
         try:
-            plugins.append(plugin.load())
+            _plugins.append(plugin.load())
         except:
             logger.warn('Unable to load BNDL plugin %r' % plugin, exc_info=True)
-    return plugins
+    return _plugins
