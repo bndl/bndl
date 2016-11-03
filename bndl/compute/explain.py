41b9af5125a4ace329a91803fd593af459b7b1ae
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-11-03 14:35:38 +0100

    clean out wip graphviz stuff for now

--- bndl/compute/explain.py
+++ bndl/compute/explain.py
@@ -1,13 +1,10 @@
 from collections import Iterable
-from tempfile import mktemp
+from functools import wraps
+import contextlib
 import inspect
 import os.path
-import traceback
-
-import graphviz
-import contextlib
 import threading
-from functools import wraps
+import traceback
 
 
 _callsite = threading.local()
@@ -94,116 +91,3 @@ def group_dsets(root):
             else:
                 stack.append(src)
     return groups
-
-# def dset_to_dot(dset):
-#     g = graphviz.Digraph()
-#     stack = [dset]
-#     while stack:
-#         dset = stack.pop()
-#         g.node(dset.id, str(dset))
-#         if isinstance(dset.src, Iterable):
-#             srcs = dset.src
-#         elif dset.src is not None:
-#             srcs = (dset.src,)
-#         else:
-#             continue
-#
-#         stack.extend(srcs)
-#         for src in srcs:
-#             g.edge(src.id, dset.id)
-#     g.render(mktemp(), '.', view=True, cleanup=True)
-
-def dset_to_dot(dset):
-    g = graphviz.Digraph()
-    g.node(dset.id, dset.name)
-
-    def add_dsets(g, sg, src, dest):
-        if isinstance(src, Iterable):
-            for src in src:
-#                 sg.node(src.id, src.name)
-#                 sg.edge(src.id, dest.id)
-#                 print(src.name, '->', dest.name)
-                add_dsets(g, sg, src, dest)
-        elif src is not None:
-            print(src.name, '->', dest.name)
-            if src.sync_required:
-#                 src = src.src
-                sg.node(src.id, src.name)
-                g.edge(src.id, dest.id, style='dashed')
-#             elif src.name != dest.name:
-            else:
-                sg.node(src.id, src.name)
-                g.edge(src.id, dest.id)
-#             else:
-#                 sg.node(src.id, src.name)
-#                 g.edge(src.id, dest.id)
-#                 sg = graphviz.Digraph()
-#                 g.subgraph(sg)
-#                 add_dsets(g, sg, src.src, dest)
-    job = group_dsets(dset)
-    for group in job:
-        sg = g
-        for dset in group:
-            add_dsets(g, g, dset.src, dset)
-    g.render(mktemp(), '.', view=True, cleanup=True)
-
-
-
-def dset_expanded_to_dot(dset):
-    g = graphviz.Digraph()
-    stack = [dset]
-    while stack:
-        dset = stack.pop()
-        g.node(str(dset))
-        if isinstance(dset.src, Iterable):
-            srcs = dset.src
-        elif dset.src is not None:
-            srcs = (dset.src,)
-        else:
-            continue
-
-        stack.extend(srcs)
-        for src in srcs:
-            g.edge(str(src), str(dset))
-    g.render(mktemp(), '.', view=True, cleanup=True)
-
-def parts_to_dot(dset):
-    g = graphviz.Digraph()
-    g.graph_attr.update(rankdir='LR')
-    stack = dset.parts()[:]
-    visited = set()
-    while stack:
-        part = stack.pop()
-        if part in visited:
-            continue
-        else:
-            visited.add(part)
-        if isinstance(part.src, Iterable):
-            srcs = part.src
-        elif part.src is not None:
-            srcs = (part.src,)
-        else:
-            continue
-        stack.extend(srcs)
-        for src in srcs:
-            g.edge(str(src), str(part))
-    g.render(mktemp(), '.', view=True, cleanup=True)
-
-def tasks_to_dot(tasks):
-    g = graphviz.Digraph()
-    g.graph_attr.update(rankdir='LR')
-    stack = tasks[:]
-    tasks = []
-    visited = set()
-    while stack:
-        task = stack.pop()
-        if task in visited:
-            continue
-        else:
-            visited.add(task)
-        g.node(str(task.part))
-        if task.dependencies:
-            stack.extend(task.dependencies)
-            for src in task.dependencies:
-                g.edge(str(src.part), str(task.part))
-    g.render(mktemp(), '.', view=True, cleanup=True)
