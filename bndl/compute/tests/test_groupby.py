4e25008be4926d2b63f7f5a8e6c2f10824e0143b
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-11-04 11:34:51 +0100

    group by now drops key

--- bndl/compute/tests/test_groupby.py
+++ bndl/compute/tests/test_groupby.py
@@ -3,13 +3,13 @@ from bndl.util.funcs import iseven
 
 
 class GroupbyTest(DatasetTest):
-    def test_groupby(self):
+    def test_group_by(self):
         self.assertEqual(self.ctx.range(10).group_by(iseven).map_values(sorted).collect(),
                          [(False, [1, 3, 5, 7, 9]), (True, [0, 2, 4, 6, 8])])
  
-    def test_groupby_key(self):
+    def test_group_by_key(self):
         dset = self.ctx.range(100).key_by(iseven).group_by_key().map_values(sorted)
         self.assertEqual(dset.collect_as_map(), {
-            False: [(False, i) for i in range(1, 100, 2)],
-            True: [(True, i) for i in range(0, 100, 2)],
+            False: list(range(1, 100, 2)),
+            True: list(range(0, 100, 2)),
         })
