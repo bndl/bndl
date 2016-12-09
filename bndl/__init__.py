47848b15f74edf72de0ac5894eaae788a23af217
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-12-09 13:08:31 +0100

    workon next dev

--- bndl/__init__.py
+++ bndl/__init__.py
@@ -6,5 +6,5 @@ if os.path.exists('logging.conf'):
     logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
 
-__version_info__ = (0, 3, 3)
+__version_info__ = (0, 3, 4, 'dev1')
 __version__ = '.'.join(map(str, __version_info__))
