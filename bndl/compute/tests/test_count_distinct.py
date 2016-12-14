8a4d1689914b96802de05e9de1fe6d1a36ba7024
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-12-14 21:23:06 +0100

    perform some extra hll tests

--- bndl/compute/tests/test_count_distinct.py
+++ bndl/compute/tests/test_count_distinct.py
@@ -1,32 +1,37 @@
 from bndl.compute.tests import DatasetTest
-from bndl.util.hyperloglog import HyperLogLog
-from bndl.compute.dataset import _as_bytes
-from random import random
+from cyhll import HyperLogLog
+from random import random, randint
 
 
 class CountDistinctTest(DatasetTest):
     def test_approx(self):
+        tvalues = [
+            [random() for _ in range(10000)],
+            [randint(0, 2 ** 32) for _ in range(10000)],
+            ['%i' % randint(0, 2 ** 32) for _ in range(10000)],
+            [b'%i' % randint(0, 2 ** 32) for _ in range(10000)],
+        ]
+
         for error_rate in (0.01, 0.05, 0.2):
-            values = [random() for _ in range(10000)]
+            for values in tvalues:
+                # perform local hll in parallel to verify
+                hll = HyperLogLog(error_rate)
+                hll.add_all(values)
 
-            # perform local hll in parallel to verify
-            hll = HyperLogLog(error_rate)
-            hll.add_all(map(_as_bytes, values))
+                for _ in range(2):
+                    # generate values
+                    extra = [random() for _ in range(941)]
+                    values += extra
 
-            for _ in range(5):
-                # generate values
-                extra = [random() for _ in range(1000)]
-                values += extra
+                    # check count accuracy
+                    count = self.ctx.collection(values).count_distinct_approx(error_rate)
+                    exact = len(set(values))
+                    delta = exact * error_rate * 4
+                    self.assertAlmostEqual(count, exact,
+                                           delta=delta,
+                                           msg='delta %s, error_rate %s' %
+                                           (abs(count - exact), error_rate))
 
-                # check count accuracy
-                count = self.ctx.collection(values).count_distinct_approx(error_rate)
-                exact = len(set(values))
-                delta = exact * error_rate * 4
-                self.assertAlmostEqual(count, exact,
-                                       delta=delta,
-                                       msg='delta %s, error_rate %s' %
-                                       (abs(count - exact), error_rate))
-
-                # check with non distributed counting
-                hll.add_all(map(_as_bytes, extra))
-                self.assertAlmostEqual(count, hll.card())
+                    # check with non distributed counting
+                    hll.add_all(extra)
+                    self.assertAlmostEqual(count, hll.card())
