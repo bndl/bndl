f1e22e90eaf97d04f4a324b3058da1e2450df65a
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-12-16 11:44:36 +0100

    default OMP_NUM_THREADS to 2 to ensure a limited number of threads is started when importing numpy

--- bndl/compute/__init__.py
+++ bndl/compute/__init__.py
@@ -4,6 +4,8 @@ The ``bndl.compute`` module provides a means to construct partitioned
 which transform and combine these data sets.
 '''
 
+import os
+
 from bndl.util import conf
 
 
@@ -13,3 +15,7 @@ pcount = conf.Int(desc='The default number of partitions. Then number of '
 worker_count = conf.Int(desc='The number of workers to start when no seeds '
                         'are given when using bndl-compute-shell or '
                         'bndl-compute-workers')
+
+
+if 'OMP_NUM_THREADS' not in os.environ:
+    os.environ['OMP_NUM_THREADS'] = '2'
