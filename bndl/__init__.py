d9553a91af3958d71720461117d8a661f243195e
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-12-30 13:13:52 +0100

    too much changes, need at least a minor version bump

--- bndl/__init__.py
+++ bndl/__init__.py
@@ -1,10 +1,8 @@
 import logging.config
 import os.path
 
-
 if os.path.exists('logging.conf'):
     logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
-
-__version_info__ = (0, 3, 5, 'dev1')
+__version_info__ = (0, 4, 0, 'dev1')
 __version__ = '.'.join(map(str, __version_info__))
