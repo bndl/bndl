50a603d1eafbd555cf8bc20238ea3e2f98353e68
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-01-04 22:51:21 +0100

    just use PyObject_Hash (and make sure PYTHONHASHSEED is set)

--- bndl/util/hash.pyx
+++ bndl/util/hash.pyx
@@ -1,65 +1,8 @@
-"""
-From https://github.com/douban/dpark/blob/master/dpark/portable_hash.pyx
-"""
-
-cdef long tuple_hash(tuple obj):
-    cdef long mul = 1000003, l = len(obj), value = 0x345678, v
-    for i in obj:
-        l -= 1
-        v = portable_hash(i)
-        if v == -1:
-            return -1
-        value = (value ^ v) * mul
-        mul += <long> (82520 + l * 2)
-    value += 97531
-    if value == -1:
-        value = -2
-    return value
-
-
-cdef long string_hash(bytes obj):
-    cdef long l = len(obj), value, i = 0, v
-    if l == 0:
-        return 0
-    v = obj[0]
-    value = v << 7
-    while i < l:
-        v = obj[i]
-        value = (1000003 * value) ^ v
-        i += 1
-
-    value ^= l
-    if value == -1:
-        value = -2
-    return value
-
-
-cdef long unicode_hash(obj):
-    cdef unicode s = obj
-    cdef long l = len(obj), value, v
-    if l == 0:
-        return 0
-    v = ord(s[0])
-    value = v << 7
-    for c in s:
-        v = ord(c)
-        value = (1000003 * value) ^ v
-
-    value ^= l
-    if value == -1:
-        value = -2
-    return value
+from cpython.object cimport PyObject_Hash
 
 
 cpdef long portable_hash(obj) except -1:
     if obj is None:
-        return 1315925605
-    t = type(obj)
-    if t is bytes:
-        return string_hash(obj)
-    elif t is unicode:
-        return unicode_hash(obj)
-    elif t is tuple:
-        return tuple_hash(obj)
+        return 0
     else:
-        return hash(obj)
+        return PyObject_Hash(obj)
