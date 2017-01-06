e6d9ecb6a43a200517b3c606afe7602a52e2b66d
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-01-06 14:10:24 +0100

    add license

--- bndl/net/sendfile.py
+++ bndl/net/sendfile.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 '''
 Adapted from and generalized into a utility outside the web context:
 https://github.com/KeepSafe/aiohttp/blob/72e615b508dc2def975419da1bddc2e3a0970203/aiohttp/web_urldispatcher.py#L439
