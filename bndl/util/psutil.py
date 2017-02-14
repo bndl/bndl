d71080b0a38648218a62910c828f6bde996206e5
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-02-14 23:25:58 +0100

    use plain * import

--- bndl/util/psutil.py
+++ bndl/util/psutil.py
@@ -10,28 +10,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import psutil
+from psutil import *
 
-
-process = psutil.Process()
+process = Process()
 process_memory_percent = process.memory_percent
 
-cpu_count = psutil.cpu_count
-cpu_percent = psutil.cpu_percent
-cpu_stats = psutil.cpu_stats
-cpu_times = psutil.cpu_times
-cpu_times_percent = psutil.cpu_times_percent
-disk_io_counters = psutil.disk_io_counters
-disk_partitions = psutil.disk_partitions
-disk_usage = psutil.disk_usage
-net_connections = psutil.net_connections
-net_if_addrs = psutil.net_if_addrs
-net_if_stats = psutil.net_if_stats
-pid_exists = psutil.pid_exists
-pids = psutil.pids
-process_iter = psutil.process_iter
-swap_memory = psutil.swap_memory
-test = psutil.test
-users = psutil.users
-virtual_memory = psutil.virtual_memory
-wait_procs = psutil.wait_procs
+
+def process_memory_percent_nonshared():
+    mem_info = process.memory_info()
+    return (mem_info.rss - mem_info.shared) / virtual_memory().total * 100
+
+# cpu_count = psutil.cpu_count
+# cpu_percent = psutil.cpu_percent
+# cpu_stats = psutil.cpu_stats
+# cpu_times = psutil.cpu_times
+# cpu_times_percent = psutil.cpu_times_percent
+# disk_io_counters = psutil.disk_io_counters
+# disk_partitions = psutil.disk_partitions
+# disk_usage = psutil.disk_usage
+# net_connections = psutil.net_connections
+# net_if_addrs = psutil.net_if_addrs
+# net_if_stats = psutil.net_if_stats
+# pid_exists = psutil.pid_exists
+# pids = psutil.pids
+# process_iter = psutil.process_iter
+# swap_memory = psutil.swap_memory
+# test = psutil.test
+# users = psutil.users
+# virtual_memory = psutil.virtual_memory
+# wait_procs = psutil.wait_procs
