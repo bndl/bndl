76cfcbb38b5fa0ed5202191bedca4bdd913e4909
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-02-17 10:03:05 +0100

    generate test methods to visualize progress in the shuffle tests

--- bndl/compute/tests/test_shuffle.py
+++ bndl/compute/tests/test_shuffle.py
@@ -22,6 +22,7 @@ import psutil
 from bndl.compute.tests import DatasetTest
 import itertools
 from math import ceil
+from _collections import OrderedDict
 
 
 logger = logging.getLogger(__name__)
@@ -35,33 +36,44 @@ class ShuffleTest(DatasetTest):
             round((200 * 1024 * 1024) / psutil.virtual_memory().total * 100, 2)
     }
 
-    def test_shuffle(self):
-        size = 2 * 1000 * 1000
-        data = self.ctx.range(size, pcount=self.worker_count * 2).map(str)
+    def _test_shuffle(self, **opts):
+        data = self.ctx.range(self.size, pcount=self.worker_count * 2).map(str)
 
-        part0_data = set(map(str, range(0, size, 2)))
-        part1_data = set(map(str, range(1, size, 2)))
 
-        sorts = [True, False]
-        serializations = ['marshal', 'pickle', 'json']
-        compressions = [None, 'gzip', 'lz4']
+        shuffled = data.shuffle(**opts) \
+                       .shuffle(**opts, pcount=3, partitioner=lambda i: int(i) % 2) \
+                       .collect(parts=True)
+        parts = list(filter(None, map(set, shuffled)))
 
-        options = itertools.product(sorts, serializations, compressions)
-        for sort, serialization, compression in options:
-            opts = dict(sort=sort, serialization=serialization, compression=compression)
-            print(opts)
-            shuffled = data.shuffle(**opts) \
-                           .shuffle(**opts, pcount=3, partitioner=lambda i: int(i) % 2) \
-                           .collect(parts=True)
-            parts = list(filter(None, map(set, shuffled)))
+        self.assertEqual(len(parts), 2)
+        self.assertEqual(len(parts[0]), self.size / 2)
+        self.assertEqual(len(parts[1]), self.size / 2)
 
-            self.assertEqual(len(parts), 2)
-            self.assertEqual(len(parts[0]), size / 2)
-            self.assertEqual(len(parts[1]), size / 2)
+        parts.sort(key=lambda part: min(part))
+        self.assertEqual(parts[0], self.part0_data)
+        self.assertEqual(parts[1], self.part1_data)
 
-            parts.sort(key=lambda part: min(part))
-            self.assertEqual(parts[0], part0_data)
-            self.assertEqual(parts[1], part1_data)
+
+sorts = [True, False]
+serializations = ['marshal', 'pickle', 'json']
+compressions = [None, 'gzip', 'lz4']
+
+options = itertools.product(sorts, serializations, compressions)
+for sort, serialization, compression in options:
+    opts = OrderedDict()
+    opts['sort'] = sort
+    opts['serialization'] = serialization
+    opts['compression'] = compression
+
+    setattr(
+        ShuffleTest,
+        'test_shuffle_' + '_'.join(map(lambda o: str(o).lower(), opts.values())),
+        lambda self, opts=opts: self._test_shuffle(**opts)
+    )
+
+    ShuffleTest.size = 2 * 1000 * 1000
+    ShuffleTest.part0_data = set(map(str, range(0, ShuffleTest.size, 2)))
+    ShuffleTest.part1_data = set(map(str, range(1, ShuffleTest.size, 2)))
 
 
 
