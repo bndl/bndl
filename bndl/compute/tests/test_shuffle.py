5e6b379d621154db8f02b745395e6d1d26f10497
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-02-17 12:25:57 +0100

    also generate test methods for the failure tests

--- bndl/compute/tests/test_shuffle.py
+++ bndl/compute/tests/test_shuffle.py
@@ -10,6 +10,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import OrderedDict
+from math import ceil
+import itertools
 import logging
 import os
 import signal
@@ -20,9 +23,7 @@ from cytoolz.itertoolz import pluck
 import psutil
 
 from bndl.compute.tests import DatasetTest
-import itertools
-from math import ceil
-from _collections import OrderedDict
+from bndl.util.collection import flatten
 
 
 logger = logging.getLogger(__name__)
@@ -36,8 +37,13 @@ class ShuffleTest(DatasetTest):
             round((200 * 1024 * 1024) / psutil.virtual_memory().total * 100, 2)
     }
 
-    def _test_shuffle(self, **opts):
-        data = self.ctx.range(self.size, pcount=self.worker_count * 2).map(str)
+
+    def _test_shuffle(self, size, **opts):
+        part0_data = set(map(str, range(0, size, 2)))
+        part1_data = set(map(str, range(1, size, 2)))
+
+        print('testing with options', opts)
+        data = self.ctx.range(size, pcount=self.worker_count * 2).map(str)
 
 
         shuffled = data.shuffle(**opts) \
@@ -46,41 +52,41 @@ class ShuffleTest(DatasetTest):
         parts = list(filter(None, map(set, shuffled)))
 
         self.assertEqual(len(parts), 2)
-        self.assertEqual(len(parts[0]), self.size / 2)
-        self.assertEqual(len(parts[1]), self.size / 2)
+        self.assertEqual(len(parts[0]), size / 2)
+        self.assertEqual(len(parts[1]), size / 2)
 
         parts.sort(key=lambda part: min(part))
-        self.assertEqual(parts[0], self.part0_data)
-        self.assertEqual(parts[1], self.part1_data)
+        self.assertEqual(parts[0], part0_data)
+        self.assertEqual(parts[1], part1_data)
 
 
-sorts = [True, False]
-serializations = ['marshal', 'pickle', 'json']
-compressions = [None, 'gzip', 'lz4']
+    @classmethod
+    def _setup_tests(cls):
+        sizes = [2 * 1000]
+        sorts = [True, False]
+        serializations = ['marshal', 'pickle', 'json']
+        compressions = [None, 'gzip', 'lz4']
 
-options = itertools.product(sorts, serializations, compressions)
-for sort, serialization, compression in options:
-    opts = OrderedDict()
-    opts['sort'] = sort
-    opts['serialization'] = serialization
-    opts['compression'] = compression
+        options = itertools.product(sizes, sorts, serializations, compressions)
+        for size, sort, serialization, compression in options:
+            opts = OrderedDict()
+            opts['size'] = size
+            opts['sort'] = sort
+            opts['serialization'] = serialization
+            opts['compression'] = compression
 
-    setattr(
-        ShuffleTest,
-        'test_shuffle_' + '_'.join(map(lambda o: str(o).lower(), opts.values())),
-        lambda self, opts=opts: self._test_shuffle(**opts)
-    )
-
-    ShuffleTest.size = 2 * 1000 * 1000
-    ShuffleTest.part0_data = set(map(str, range(0, ShuffleTest.size, 2)))
-    ShuffleTest.part1_data = set(map(str, range(1, ShuffleTest.size, 2)))
+            setattr(
+                ShuffleTest,
+                'test_shuffle_' + '_'.join(map(lambda o: str(o).lower(), opts.values())),
+                lambda self, opts=opts: self._test_shuffle(**opts)
+            )
 
 
 
 class ShuffleFailureTest(DatasetTest):
     worker_count = 20
 
-    def test_dependency_failure(self):
+    def _test_dependency_failure(self, dset_size, pcount, key_count, kill_after):
         class WorkerKiller(object):
             def __init__(self, worker, count):
                 self.count = count + 1
@@ -107,6 +113,33 @@ class ShuffleFailureTest(DatasetTest):
             identity_mapper(killers, key_count, i)
             return (i % key_count, i)
 
+        try:
+            self.ctx.conf['bndl.execute.attempts'] = 2
+
+            killers = [
+                self.ctx.accumulator(WorkerKiller(worker, count))
+                for worker, count in zip(self.ctx.workers, kill_after)
+            ]
+
+            dset = self.ctx \
+                       .range(dset_size, pcount=pcount) \
+                       .map(identity_mapper, killers, key_count) \
+                       .shuffle() \
+                       .map(keyby_mapper, killers, key_count) \
+                       .aggregate_by_key(sum)
+
+            result = dset.collect()
+            self.assertEqual(len(result), key_count)
+            self.assertEqual(sorted(pluck(0, result)), list(range(key_count)))
+            self.assertEqual(sum(pluck(1, result)), sum(range(dset_size)))
+
+            time.sleep(1)
+        finally:
+            self.ctx.conf['bndl.execute.attempts'] = 1
+
+
+    @classmethod
+    def _setup_tests(cls):
         # dset_size, pcount, key_count, killers
         test_cases = [
             [100, 30, 30, [125, 125, 125]],
@@ -117,27 +150,11 @@ class ShuffleFailureTest(DatasetTest):
             [ 10, 3, 3, [0]],
         ]
 
-        try:
-            for dset_size, pcount, key_count, kill_after in test_cases:
-                self.ctx.conf['bndl.execute.attempts'] = 2
+        for dset_size, pcount, key_count, kill_after in test_cases:
+            args = (dset_size, pcount, key_count, kill_after)
+            name = 'test_dependency_failure_' + '_'.join(map(str, flatten(args)))
+            test = lambda self, args = args:self._test_dependency_failure(*args)
+            setattr(cls, name, test)
 
-                killers = [
-                    self.ctx.accumulator(WorkerKiller(worker, count))
-                    for worker, count in zip(self.ctx.workers, kill_after)
-                ]
-
-                dset = self.ctx \
-                           .range(dset_size, pcount=pcount) \
-                           .map(identity_mapper, killers, key_count) \
-                           .shuffle() \
-                           .map(keyby_mapper, killers, key_count) \
-                           .aggregate_by_key(sum)
-
-                result = dset.collect()
-                self.assertEqual(len(result), key_count)
-                self.assertEqual(sorted(pluck(0, result)), list(range(key_count)))
-                self.assertEqual(sum(pluck(1, result)), sum(range(dset_size)))
-
-                time.sleep(1)
-        finally:
-            self.ctx.conf['bndl.execute.attempts'] = 1
+ShuffleTest._setup_tests()
+ShuffleFailureTest._setup_tests()
