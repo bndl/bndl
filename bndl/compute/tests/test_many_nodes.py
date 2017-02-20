f95e0cecfdd3b6de0a15d4597041993aacc21128
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-02-20 15:30:19 +0100

    the mean of 1-100 gets rounded differently depending on the partial answers

--- bndl/compute/tests/test_many_nodes.py
+++ bndl/compute/tests/test_many_nodes.py
@@ -17,4 +17,4 @@ class ManyNodesTest(DatasetTest):
     worker_count = 8
 
     def test_many_nodes(self):
-        self.assertEqual(self.ctx.range(1, 100).mean(), 50)
+        self.assertAlmostEqual(self.ctx.range(1, 100).mean(), 50)
