9f674c2e0b6dae881ffc030a03d2b4df669ab4ee
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-03-23 17:50:40 +0100

    templates moved to compute prefix

--- bndl/util/dash/settings.py
+++ bndl/util/dash/settings.py
@@ -15,5 +15,5 @@ SECRET_KEY = 'default'
 BNDL_DASHES = [
     ('docs', 'bndl.util.dash.docs.Dash'),
     ('net', 'bndl.net.dash.Dash'),
-    ('execute', 'bndl.compute.dash.Dash'),
+    ('compute', 'bndl.compute.dash.Dash'),
 ]
