1b2a1bb5f4c99f80c3664a40796939732e9fe91c
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2017-05-11 22:25:57 +0200

    bump dev version

--- bndl/__init__.py
+++ bndl/__init__.py
@@ -35,5 +35,5 @@ if os.path.exists(conf['bndl.logging_conf']):
 
 # BNDL version info
 
-__version_info__ = (0, 6, 0)
+__version_info__ = (0, 7, 0, 'dev2')
 __version__ = '.'.join(map(str, __version_info__))
